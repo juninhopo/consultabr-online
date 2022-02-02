b336da216c86bbfe033186d58ee724f01810a868
juninhopo <kelgaxd@gmail.com>
2022-02-02 20:01:43 -0300

    update app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1 id="titulo">Confira CEP</h1>
+      <div>
+        
+        <button id="checar">Checar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
