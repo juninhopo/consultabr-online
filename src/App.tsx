b85e84020aa9321d223091744a3a37c8afa841be
juninhopo <kelgaxd@gmail.com>
2022-02-02 20:07:09 -0300

    add button Confira

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { ButtonConfira } from "./components/button";
+
 function App() {
   return (
     <div>
       <h1 id="titulo">Confira CEP</h1>
       <div>
-        
-        <button id="checar">Checar</button>
+      
+        <ButtonConfira />
       </div>
     </div>
   );
---
+++ src/components/button.tsx
@@ -0,0 +1,5 @@
+export function ButtonConfira () {
+    return (
+        <button>Confira</button>
+    )
+}
