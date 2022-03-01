7bb568e1f639041bfaf5bc2d9d36ea7499c30453
filipedev8 <eulerfilipe8@mail.com>
2022-03-01 18:23:12 -0300

    avaço

--- controllers/script.js
+++ controllers/script.js
@@ -1,8 +1,5 @@
 function overlayHander(fechar) {
     const overlay = document.getElementById("overlay")
     overlay.style.display = fechar ? "none": "flex";
-    alert(1)
-
-    console.log(overlay)
 }
 
