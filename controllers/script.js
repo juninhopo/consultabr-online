a4fbe62b59f94760371993483dd261385c5f796b
filipedev8 <eulerfilipe8@mail.com>
2022-03-01 18:07:34 -0300

    sricpt.js

---
+++ controllers/script.js
@@ -0,0 +1,8 @@
+function overlayHander(fechar) {
+    const overlay = document.getElementById("overlay")
+    overlay.style.display = fechar ? "none": "flex";
+    alert(1)
+
+    console.log(overlay)
+}
+
